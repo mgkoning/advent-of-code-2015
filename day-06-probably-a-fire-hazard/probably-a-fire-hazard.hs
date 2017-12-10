da2ea98d61fa423925339b8d74457459056d030e
Michiel Koning <m.g.koning@gmail.com>
2017-12-10 01:13:39 +0100

    day 6: probably a fire hazard. Fairly slow. Rewrote to use int for part 2.

--- day-06-probably-a-fire-hazard/probably-a-fire-hazard.hs
+++ day-06-probably-a-fire-hazard/probably-a-fire-hazard.hs
@@ -2,14 +2,38 @@ import Data.Function
 import Data.List
 import qualified Data.Map.Strict as Map
 
-data LightState = On | Off deriving Show
+makeInitialState :: Map.Map (Int, Int) Int
+makeInitialState = Map.fromList [((x, y), 0) | x <- [0..999], y <- [0..999]]
 
-toggle :: LightState -> LightState
-toggle On = Off
-toggle Off = On
+makeNextState :: (Int -> Int) -> (Int -> Int) -> (Int -> Int) -> Map.Map (Int, Int) Int -> [String] -> Map.Map (Int, Int) Int
+makeNextState toggle turnOn turnOff state instruction =
+  case instruction of
+    ("toggle":from:"through":through:[]) ->
+      let (fromX, fromY) = readCoord from
+          (toX, toY) = readCoord through
+          range = [(x, y) | x <- [fromX..toX], y <- [fromY..toY]]
+      in foldl' (flip (Map.adjust toggle)) state range
+    ("turn":lightState:from:"through":through:[]) ->
+      let (fromX, fromY) = readCoord from
+          (toX, toY) = readCoord through
+          range = [(x, y) | x <- [fromX..toX], y <- [fromY..toY]]
+          op = if lightState == "on" then turnOn else turnOff
+      in foldl' (flip (Map.adjust op)) state range
 
-makeInitialState :: Map.Map (Int, Int) LightState
-makeInitialState = Map.fromList [((x, y), Off) | x <- [0..999], y <- [0..999]]
+readCoord :: String -> (Int, Int)
+readCoord s = let (a, b) = span (/=',') s in (read a, read $ tail b)
 
-readInstruction ("turn":state:from:"through":through:[]) = 0
-readInstruction ("toggle":from:"through":through:[]) = 0
+runInstructions1 :: [[String]] -> Map.Map (Int, Int) Int
+runInstructions1 = foldl' (makeNextState (abs . subtract 1) (const 1) (const 0)) makeInitialState 
+
+runInstructions2 :: [[String]] -> Map.Map (Int, Int) Int
+runInstructions2 = foldl' (makeNextState (+2) (+1) (max 0 . subtract 1)) makeInitialState 
+
+solve = do
+  input <- readFile "input.txt"
+  let instructions = map words $ lines input
+  let result = runInstructions1 instructions
+  putStrLn "Part 1:"
+  putStrLn $ show $ length $ filter (>0) $ Map.elems result
+  putStrLn "\nPart 2:"
+  putStrLn $ show $ sum $ Map.elems $ runInstructions2 instructions
