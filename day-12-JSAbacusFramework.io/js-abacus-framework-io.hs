fc0a4b0f1e403089d8a2f6bde10b2f84f0d3c35e
Michiel Koning <m.g.koning@gmail.com>
2017-12-23 18:27:50 +0100

    day 12: JSAbacusFramework.io

--- day-12-JSAbacusFramework.io/js-abacus-framework-io.hs
+++ day-12-JSAbacusFramework.io/js-abacus-framework-io.hs
@@ -1,6 +1,7 @@
 import Control.Applicative (empty)
 import qualified Numeric as Numeric
 import Text.ParserCombinators.Parsec
+import Data.Either
 
 data JValue = JObject [(String, JValue)] | JArray [JValue] | JString String | JInteger Int deriving (Show)
 
@@ -23,7 +24,7 @@ readSeries left parser right =
 
 readValue :: CharParser () JValue
 readValue = value <* spaces
-  where value = choice [ JString <$> readString , JInteger <$> readInteger ] <?> "JSON value"
+  where value = choice [ JString <$> readString , JInteger <$> readInteger, readObject, readArray ] <?> "JSON value"
 
 readInteger = do s <- getInput
                  case Numeric.readSigned Numeric.readDec s of
@@ -32,3 +33,21 @@ readInteger = do s <- getInput
 
 readString = between (char '\"') (char '\"') (many jchar)
   where jchar = satisfy (/= '\"')
+
+calculateSum ignoreRed jValue =
+  case jValue of
+    JInteger x -> x
+    JString x -> 0
+    JArray xs -> sum $ map (calculateSum ignoreRed) xs
+    JObject xs ->
+      let skip = ignoreRed && (or $ map isRed xs)
+          isRed (_, JString "red") = True
+          isRed _ = False
+      in if skip then 0 else sum $ map (calculateSum ignoreRed . snd) xs
+
+solve = do
+  json <- (parse json "input.txt") <$> readFile "input.txt"
+  putStrLn "Part 1:"
+  putStrLn $ show $ calculateSum False $ head $ rights [json]
+  putStrLn "\nPart 2:"
+  putStrLn $ show $ calculateSum True $ head $ rights [json]
